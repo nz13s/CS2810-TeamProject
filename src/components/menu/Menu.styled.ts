0e96a4d54f915bf511b479777f379d39e5d66fa3
chebyrash <pd.stroganov@gmail.com>
2020-01-21 13:55:19 +0000

    Added menu prototype

---
+++ src/components/menu/Menu.styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const MenuStyle = styled.div`
+  display: flex;
+`;
+
+
+export {MenuStyle};
---
+++ src/entities/MenuItem.ts
@@ -0,0 +1,15 @@
+export default class MenuItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+
+  constructor(id: number, name: string, description: string, price: number, image: string) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+  }
+}
