00d7408a39d1772a4705a436d59fa1a63ea21d48
cameron <zfac034@rhul.ac.uk>
2020-01-25 17:06:21 +0000

    Added servlet to handle saving a Basket to the database ready for @Oliver to connect

---
+++ src/main/java/SaveBasket.java
@@ -0,0 +1,46 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class for handling modification and access to a sessions {@link Basket}
+ */
+@WebServlet("/save")
+public class SaveBasket extends HttpServlet {
+
+    /**
+     * Saves the sessions {@link Basket} to the database
+     *
+     * @param req  The {@link HttpServletRequest} object that contains the request the client made of the servlet
+     * @param resp The {@link HttpServletResponse} object that contains the response the servlet returns to the client
+     * @throws ServletException
+     * @throws IOException      If an input or output exception occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Basket basket = getBasket(req);
+        if (basket != null) {
+            boolean allGood = true;
+            //TODO @Oliver please
+            //saveBasket returns boolean
+            if (!allGood/*DB.saveBasket(basket)*/) {
+                resp.sendError(500, "Unable to save basket.");
+            }
+        } else {
+            resp.sendError(500, "No order exists for this session.");
+        }
+    }
+
+    /**
+     * Gets the sessions {@link Basket} returns null if no Basket exists
+     *
+     * @param req The {@link HttpServletRequest} object that contains the request the client made of the servlet
+     * @return The sessions Basket or null if none exists
+     */
+    private Basket getBasket(HttpServletRequest req) {
+        return (Basket) req.getSession().getAttribute("basket");
+    }
+}
