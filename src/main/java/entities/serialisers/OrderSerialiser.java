f3e2128b8e37d21ac0264aa8de2d6ad565adee86
cameron <zfac034@rhul.ac.uk>
2020-01-27 23:20:35 +0000

    Created Class to serialise Orders for the Frontend

---
+++ src/main/java/entities/serialisers/OrderSerialiser.java
@@ -0,0 +1,30 @@
+package entities.serialisers;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import entities.Order;
+
+import java.io.IOException;
+
+/**
+ * Class to serialise a {@link Order} to pass to the Frontend
+ */
+public class OrderSerialiser extends StdSerializer<Order> {
+
+    public OrderSerialiser(){
+        this(null);
+    }
+
+    public OrderSerialiser(Class<Order> order) {
+        super(order);
+    }
+
+    @Override
+    public void serialize(
+            Order order, JsonGenerator jsonGenerator, SerializerProvider serializer) throws IOException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeObjectField("items", order.getFoodItems());
+        jsonGenerator.writeEndObject();
+    }
+}
