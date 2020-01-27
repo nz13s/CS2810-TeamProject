7c33f5faeee293379e74a09d18b12ef068a4ebc5
cameron <zfac034@rhul.ac.uk>
2020-01-27 23:22:27 +0000

    Changed AddToBasket class to use the Order class instead of the basket class

--- src/main/java/endpoints/AddToBasket.java
+++ src/main/java/endpoints/AddToBasket.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import entities.Basket;
 import entities.Item;
-import entities.serialisers.BasketSerialiser;
+import entities.Order;
+import entities.serialisers.OrderSerialiser;
 
 import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
@@ -16,33 +16,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.sql.SQLException;
 
 /**
- * Class for handling modification and access to a sessions {@link Basket}
+ * Class for handling modification and access to a sessions {@link Order}
  */
-@WebServlet("/baskets")
+@WebServlet("/order")
 public class AddToBasket extends HttpServlet {
 
     //https://www.baeldung.com/jackson-object-mapper-tutorial
     private ObjectMapper om = new ObjectMapper();
 
     /**
-     * Configures the {@link ObjectMapper} before being used to serialise the {@link Basket} for the frontend
+     * Configures the {@link ObjectMapper} before being used to serialise the {@link Order} for the frontend, uses the {@link OrderSerialiser}
      * @throws ServletException
      */
     @Override
     public void init() throws ServletException {
         om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         SimpleModule module =
-                new SimpleModule("BasketSerialiser", new Version(1, 0, 0, null, null, null));
-        module.addSerializer(Basket.class, new BasketSerialiser());
+                new SimpleModule("OrderSerialiser", new Version(1, 0, 0, null, null, null));
+        module.addSerializer(Order.class, new OrderSerialiser());
         om.registerModule(module);
     }
 
     /**
-     * Returns the sessions {@link Basket}
+     * Returns the sessions {@link Order}
      * @param req The {@link HttpServletRequest} object that contains the request the client made of the servlet
      * @param resp The {@link HttpServletResponse} object that contains the response the servlet returns to the client
      * @throws ServletException
@@ -50,16 +49,16 @@ public class AddToBasket extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Basket basket = getBasket(req);
+        Order order = getOrder(req);
         resp.reset();
         resp.setContentType("application/json");
         PrintWriter pw = resp.getWriter();
-        pw.println(om.writeValueAsString(basket));
+        pw.println(om.writeValueAsString(order));
         pw.flush();
     }
 
     /**
-     * Adds the specified amount of the specified {@link Item} from the sessions {@link Basket}
+     * Adds the specified amount of the specified {@link Item} from the sessions {@link Order}
      * @param req The {@link HttpServletRequest} object that contains the request the client made of the servlet
      * @param resp The {@link HttpServletResponse} object that contains the response the servlet returns to the client
      * @throws ServletException
@@ -73,20 +72,16 @@ public class AddToBasket extends HttpServlet {
         } catch (NumberFormatException e) {
             return;
         }
-        Basket basket = getBasket(req);
+        Order order = getOrder(req);
         try {
-            basket.addToBasket(IDAmount[0], IDAmount[1]);
+            order.addFoodItem(IDAmount[0], IDAmount[1]);
         } catch (SQLException e) {
-//            StringWriter strW = new StringWriter();
-//            e.printStackTrace(new PrintWriter(strW));
-//            strW.flush();
             resp.sendError(400, "Item not found");
-//            strW.close();
         }
     }
 
     /**
-     * Deletes the specified amount of the specified {@link Item} from the sessions {@link Basket}
+     * Deletes the specified amount of the specified {@link Item} from the sessions {@link Order}
      * @param req The {@link HttpServletRequest} object that contains the request the client made of the servlet
      * @param resp The {@link HttpServletResponse} object that contains the response the servlet returns to the client
      * @throws ServletException
@@ -100,8 +95,8 @@ public class AddToBasket extends HttpServlet {
         } catch (NumberFormatException e) {
             return;
         }
-        Basket basket = getBasket(req);
-        basket.removeFromBasket(IDAmount[0], IDAmount[1]);
+        Order order = getOrder(req);
+        order.removeFoodItem(IDAmount[0], IDAmount[1]);
     }
 
     /**
@@ -134,17 +129,17 @@ public class AddToBasket extends HttpServlet {
     }
 
     /**
-     * Gets the sessions {@link Basket}, if no Basket is found an empty one is created
+     * Gets the sessions {@link Order}, if no Order is found an empty one is created
      * @param req The {@link HttpServletRequest} object that contains the request the client made of the servlet
-     * @return The sessions Basket or a newly created empty one
+     * @return The sessions Order or a newly created empty one
      */
     @Nonnull
-    private Basket getBasket(HttpServletRequest req) {
-        Basket basket = (Basket) req.getSession().getAttribute("basket");
-        if (basket == null) {
-            basket = new Basket();
-            req.getSession().setAttribute("basket", basket);
+    private Order getOrder(HttpServletRequest req) {
+        Order order = (Order) req.getSession().getAttribute("order");
+        if (order == null) {
+            order = new Order();
+            req.getSession().setAttribute("order", order);
         }
-        return basket;
+        return order;
     }
 }
