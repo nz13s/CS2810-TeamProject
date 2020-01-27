a1aa3962065e4afb0583fb96cbf4ce626da866ea
Jatin Khatra <jatinkhatra13@gmail.com>
2020-01-27 11:05:39 +0000

    Fixed FoodIngredientsSql queries in response to the database changes. Added Javadoc to the file as well.

--- src/main/java/sql/FoodIngredientsSql.java
+++ src/main/java/sql/FoodIngredientsSql.java
@@ -16,43 +16,43 @@ import java.sql.SQLException;
  */
 
 public class FoodIngredientsSql {
-  private PreparedStatement getIngredients;
-  private PreparedStatement s;
+    private PreparedStatement getIngredients;
+    private PreparedStatement s;
 
-  /**
-   * Constructor that holds the SQL queries that are going to be used.
-   *
-   * @author Jatin
-   * @param connection connection to the database.
-   * @throws SQLException thrown if sql logic is wrong.
-   */
+    /**
+     * Constructor that holds the SQL queries that are going to be used.
+     *
+     * @param connection connection to the database.
+     * @throws SQLException thrown if sql logic is wrong.
+     * @author Jatin
+     */
 
-  public FoodIngredientsSql(Connection connection) throws SQLException {
+    public FoodIngredientsSql(Connection connection) throws SQLException {
 
-    getIngredients = connection.prepareStatement(
-            "SELECT ingredient_id, ingredient "
-            + "FROM ingredients, food "
-            + "WHERE (ingredients.ingredient_id = food.food_id AND food_id = ?)");
-  }
-
-  /**
-   * Method that gets an ingredient based on the foodID.
-   *
-   * @param foodID foodID of the food.
-   * @return FoodIngredients object, based on the sql query output.
-   * @throws SQLException thrown if sql logic is wrong.
-   */
-
-  @CheckForNull
-  @CheckReturnValue
-  public FoodIngredients getIngredientsById(int foodID) throws SQLException {
-    getIngredients.setInt(1, foodID);
-    ResultSet resultSet = getIngredients.executeQuery();
-    if (resultSet.next()) {
-      return new FoodIngredients(
-          resultSet.getInt("food_id"),
-          resultSet.getString("ingredient"));
+        getIngredients = connection.prepareStatement(
+                "SELECT ingredient_id, ingredient "
+                        + "FROM ingredients, food "
+                        + "WHERE (ingredients.ingredient_id = food.food_id AND food_id = ?)");
     }
-    return null;
-  }
+
+    /**
+     * Method that gets an ingredient based on the foodID.
+     *
+     * @param foodID foodID of the food.
+     * @return FoodIngredients object, based on the sql query output.
+     * @throws SQLException thrown if sql logic is wrong.
+     */
+
+    @CheckForNull
+    @CheckReturnValue
+    public FoodIngredients getIngredientsById(int foodID) throws SQLException {
+        getIngredients.setInt(1, foodID);
+        ResultSet resultSet = getIngredients.executeQuery();
+        if (resultSet.next()) {
+            return new FoodIngredients(
+                    resultSet.getInt("food_id"),
+                    resultSet.getString("ingredient"));
+        }
+        return null;
+    }
 }
