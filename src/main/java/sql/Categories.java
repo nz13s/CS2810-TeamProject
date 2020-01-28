1615704e49d36b600f35d04362f70d171784d715
Jatin Khatra <jatinkhatra13@gmail.com>
2020-01-28 11:42:11 +0000

    Fixed Javadoc and removed code that was not used.

--- src/main/java/sql/Categories.java
+++ src/main/java/sql/Categories.java
@@ -12,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -47,10 +46,6 @@ public class Categories {
                         + "FROM food "
                         + "WHERE category_id = ?");
 
-        getAllCatId = connection.prepareStatement(
-                "SELECT category_id "
-                        + " FROM categories");
-
         fetchMenu = connection.prepareStatement(
                 "SELECT food_id, food_name, food_description, calories, price, available, category, c.category_id " +
                         "FROM food " +
@@ -73,7 +68,6 @@ public class Categories {
         catById.setInt(1, categoryID);
         ResultSet resultSet = catById.executeQuery();
         if (resultSet.next()) {
-            //return new entities.Category(resultSet.getInt("category_id"), resultSet.getString("category"));
             return new Category(1,"error");
         }
         return null;
@@ -106,6 +100,14 @@ public class Categories {
         return list;
     }
 
+    /**
+     * Method that organises all categories and foods into an ArrayList to represent
+     * the restaurant's menu, and stores all information into a Menu object.
+     *
+     * @return Menu object that holds all categories and foods, based on the database.
+     * @throws SQLException thrown if sql logic is wrong.
+     */
+
     @Nonnull
     public Menu fetchMenu() throws SQLException {
         Menu menu = new Menu();
@@ -126,46 +128,6 @@ public class Categories {
             categories.add(c);
         }
         categories.forEach(menu::addCat);
-        return menu;
-    }
-
-    /**
-     * Method that organises all categories and foods into an ArrayList to represent
-     * the restaurant's menu, and stores all information into a Menu object.
-     *
-     * @return Menu object that holds all categories and foods, based on the database.
-     * @throws SQLException thrown if sql logic is wrong.
-     */
-
-    public Menu getMenu() throws SQLException {
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        ResultSet resultSet = getAllCatId.executeQuery();
-
-        // adds all category numbers into an ArrayList.
-        while (resultSet.next()) {
-            nums.add(resultSet.getInt("category_id"));
-        }
-
-        Menu menu = new Menu();
-
-        // loop through all categoryIDs.
-        for (int i = 0; i < nums.size(); i++) {
-            // make a temp Category object.
-            // Foods that relate to this Category will be added.
-            Category temp = getCatByID(nums.get(i));
-
-            // ArrayList that holds all Food objects for the temp Category.
-            ArrayList<Food> list = getFoodByCatId(nums.get(i));
-
-            // loop that adds each Food object to the temp category.
-            for (int j = 0; j < list.size(); j++) {
-                temp.addFood(list.get(j));
-            }
-
-            // temp category is added to a Menu object.
-            menu.addCat(temp);
-        }
-
         return menu;
     }
 
