77f7ab0c7c50def954244777c43fc8467e69c02d
Bhavik Narang <bhavik.narang99@gmail.com>
2020-02-04 13:35:20 +0000

    SQl query now returns the foods that are part of an order and joins them according to the orderID

--- src/main/java/sql/Tables.java
+++ src/main/java/sql/Tables.java
@@ -15,6 +15,7 @@ import java.util.Set;
  * Class that stores SQL queries related to the restaurant_table in the database.
  *
  * @author Jatin Khatra
+ * @author Bhavik Narang
  */
 
 public class Tables {
@@ -31,10 +32,10 @@ public class Tables {
     public Tables(Connection connection) throws SQLException {
 
         fetchTables = connection.prepareStatement(
-                "SELECT restaurant_table.table_num, seats_available, occupied, order_id, time_ordered, order_confirmed, order_preparing, order_ready, order_served " +
-                        "FROM restaurant_table, orders " +
-                        "WHERE orders.table_num = restaurant_table.table_num " +
-                        "ORDER BY table_num ASC");
+                "SELECT restaurant_table.table_num, seats_available, occupied, orders.order_id, time_ordered, order_confirmed, order_preparing, order_ready, order_served, food.food_name, food.price, food_orders.quantity " +
+                        "FROM restaurant_table join orders on restaurant_table.table_num = orders.table_num, food_orders join food on food_orders.food_id = food.food_id " +
+                        "WHERE orders.table_num = restaurant_table.table_num AND food_orders.order_id = orders.order_id " +
+                        "ORDER BY table_num");
     }
 
     /**
