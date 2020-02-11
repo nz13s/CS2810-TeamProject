0795ba307cb46c6553de0ca9a7caec46503567ca
oliver <Oliver.Graham.2018@rhul.ac.uk>
2020-02-11 00:50:14 +0000

    Refractor HeaderSessionFilter out of DatabaseConnectionFilter

--- src/main/java/filters/DatabaseConnectionFilter.java
+++ src/main/java/filters/DatabaseConnectionFilter.java
@@ -3,16 +3,13 @@ package filters;
 import databaseInit.Database;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.sql.SQLException;
 
-@WebFilter("/*")
 public class DatabaseConnectionFilter implements Filter {
 
     private String cachedError;
@@ -50,15 +47,5 @@ public class DatabaseConnectionFilter implements Filter {
             return;
         }
 
-        SessionRepositoryRequestWrapper wrapper = new SessionRepositoryRequestWrapper(req);
-        HttpSession session = wrapper.getSession();
-        if (req.getServletPath().equalsIgnoreCase("/hello")){
-            session = wrapper.genSession();
-        } else if (wrapper.getSession(false) == null){
-            resp.sendError(401,"You need to provide your authentication token via the X-Session-ID. You can initiate a session with /hello");
-            return;
-        }
-        resp.setHeader("X-Session-ID", session.getId());
-        chain.doFilter(wrapper, response);
     }
 }
---
+++ src/main/java/filters/HeaderSessionFilter.java
@@ -0,0 +1,28 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class HeaderSessionFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        SessionRepositoryRequestWrapper wrapper = new SessionRepositoryRequestWrapper(req);
+        HttpSession session = wrapper.getSession();
+        if (req.getServletPath().equalsIgnoreCase("/hello")) {
+            session = wrapper.genSession();
+        } else if (wrapper.getSession(false) == null) {
+            resp.sendError(401, "You need to provide your authentication token via the X-Session-ID. You can initiate a session with /hello");
+            return;
+        }
+        resp.setHeader("X-Session-ID", session.getId());
+        chain.doFilter(wrapper, response);
+    }
+}
