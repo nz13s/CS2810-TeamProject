f7198941be6386b4b7a2684788934ba304518e4d
Jatin Khatra <jatinkhatra13@gmail.com>
2020-02-12 14:06:10 +0000

    Added method that adds the table notification to all active staff's notifications. Changed the attribute to hold a List of active staff.

--- src/main/java/notificationLogic/TableNotificationToWaiters.java
+++ src/main/java/notificationLogic/TableNotificationToWaiters.java
@@ -1,13 +1,28 @@
 package notificationLogic;
 
 import entities.ActiveStaff;
+import entities.Notification;
+import entities.StaffInstance;
+import entities.Table;
+
+import java.util.List;
 
 public class TableNotificationToWaiters {
 
-    private ActiveStaff staff;
+    private List<StaffInstance> staff;
 
     public TableNotificationToWaiters() {
-        staff = new ActiveStaff();
+        staff = new ActiveStaff().getAllActiveStaff();
+    }
+
+    public void addTableNotificationToAllStaff() {
+
+        //temp table object - will be changed to actual table object depending on how tables are going to see assigned.
+        Table table = new Table(1,1,false,null);
+
+        for (StaffInstance staffInstance : staff) {
+            staffInstance.addNotification(new Notification(table, "Table waiting for order confirmation"));
+        }
     }
 
 }
