e2bd65e1ebf8bc02bd46685114dead02fc9efb10
Bhavik Narang <bhavik.narang99@gmail.com>
2020-02-21 11:34:32 +0000

    Order now updates to show when order has been served.

--- src/main/java/endpoints/restricted/StaffNotifications.java
+++ src/main/java/endpoints/restricted/StaffNotifications.java
@@ -1,15 +1,20 @@
 package endpoints.restricted;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import databaseInit.Database;
 import entities.NotificationTypes;
+import entities.Order;
 import entities.StaffInstance;
 
+import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Objects;
 
 /**
  * Endpoint for the frontend to call to get the notifications for a sessions {@link StaffInstance}
@@ -61,6 +66,21 @@ public class StaffNotifications extends HttpServlet {
             resp.sendError(400, "Invalid ID.");
             return;
         }
+        if (type==NotificationTypes.READY){
+            Order o = null;
+            try {
+                o = getOrder(req);
+            } catch (SQLException e) {
+                resp.sendError(500, "Could not get Order.");
+            }
+            try {
+                Database.ORDERS.updateOrderServedByID(Objects.requireNonNull(o).getOrderID());
+            }  catch (SQLException e) {
+                resp.sendError(500, "Unable to update order.");
+            } catch (NullPointerException d) {
+                resp.sendError(500, "Could not get Order.");
+            }
+        }
         StaffInstance staff = getStaffMember(req);
         staff.removeNotificationByID(ID);
     }
@@ -81,4 +101,19 @@ public class StaffNotifications extends HttpServlet {
         }
         return staff;
     }
+
+    /**
+     * Gets the orderID from frontend as parameter.
+     *
+     * @param req The {@link HttpServletRequest} object that contains the request the client made of the servlet
+     * @return The sessions Basket or null if none exists
+     */
+    @Nonnull
+    private Order getOrder(HttpServletRequest req) throws SQLException {
+        int orderID = Integer.parseInt(req.getParameter("orderID"));
+        Order order = Database.ORDERS.getOrderByID(orderID);
+        assert order != null;
+        return order;
+    }
 }
+
