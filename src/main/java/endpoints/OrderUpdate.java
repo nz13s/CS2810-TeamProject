41fbe0b6a87a72f6b1b5f748d7be01baeb960408
tonydelchev <tanimirdelchev@gmail.com>
2020-02-23 22:32:20 +0000

    Implemented a different solution to sending a notification to Waiter assigned to that table, now tables hold a waiter field if table has no waiter a waiter is assigned.

--- src/main/java/endpoints/OrderUpdate.java
+++ src/main/java/endpoints/OrderUpdate.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
 
 
 /**
@@ -53,29 +52,14 @@ public class OrderUpdate extends HttpServlet {
             success = Database.ORDERS.updateOrderState(order, state);
             if (state == 2) {
                 Table orderTable = Database.TABLES.getTableByID(order.getTableNum());
-                Notification notifReady = new Notification(orderTable, NotificationTypes.READY);
-
-                //check every staff member's list.
-                //if they have a notification that matches the tableNum of the order's table,
-                //then their staff ID should be acquired.
-
+                Notification nfReady = new Notification(orderTable, NotificationTypes.READY);
                 assert orderTable != null;
-                int tableNum = orderTable.getTableNum();
-                int staffID = 0;
 
-                List<StaffInstance> allStaff = ActiveStaff.getAllActiveStaff();
-
-                for (StaffInstance staffInstance : allStaff) {
-                    List<Notification> oneStaffNotification = staffInstance.getNotifications();
-                    for (Notification notification : oneStaffNotification) {
-                        if (notification.getTable().getTableNum() == tableNum) {
-                            staffID = staffInstance.getStaffID();
-                            break;
-                        }
-                    }
+                if (orderTable.getWaiter() == null) {
+                    ActiveStaff.addTableToStaff(orderTable);
                 }
 
-                ActiveStaff.addNotification(staffID, notifReady);
+                ActiveStaff.addNotification(orderTable.getWaiter(), nfReady);
             }
         } catch (SQLException e) {
             resp.sendError(500, "Unable to update order.");
