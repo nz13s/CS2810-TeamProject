ed237ada8e95c032763ac54a7631f548edc55a55
tonydelchev <tanimirdelchev@gmail.com>
2020-02-23 22:36:04 +0000

    Changed the Assistance Notification from Random selection to the waiter assigned with table, if no waiter a waiter is assigned.

--- src/main/java/endpoints/NotifyWaiter.java
+++ src/main/java/endpoints/NotifyWaiter.java
@@ -2,41 +2,40 @@
 package endpoints;
 
 import databaseInit.Database;
-import entities.*;
+import entities.ActiveStaff;
+import entities.Notification;
+import entities.NotificationTypes;
+import entities.Table;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Random;
 
-//TODO -- Tony Validation / Documentation and finishing off class.
 public class NotifyWaiter extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        Table t = null;
+            throws IOException {
+        Table table = null;
         try {
-            t = Database.TABLES.getTableByID(getTable(req, resp));
+            table = Database.TABLES.getTableByID(getTable(req, resp));
         } catch (SQLException e) {
             resp.sendError(500, "Error getting TableID from database.");
         }
 
-        if (t != null) {
-            Notification n = new Notification(t, NotificationTypes.ASSIST);
-            List<StaffInstance> active = ActiveStaff.getAllActiveStaff();
-            Random rand = new Random();
-
-            StaffInstance waiter = active.get(rand.nextInt(active.size()));
-            ActiveStaff.addNotification(waiter.getStaffID(), n);
+        if (table != null) {
+            Notification n = new Notification(table, NotificationTypes.ASSIST);
+            if (table.getWaiter() == null) {
+                ActiveStaff.addTableToStaff(table);
+            }
+            ActiveStaff.addNotification(table.getWaiter(), n);
         } else {
             resp.sendError(500, "Null value tableID");
         }
 
     }
+
     private int getTable(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int tableID = -1;
         try {
@@ -49,6 +48,4 @@ public class NotifyWaiter extends HttpServlet {
         }
         return tableID;
     }
-
-
 }
