16b0e01aa4ff6e0fc76274b976ca870ded6ea123
chebyrash <pd.stroganov@gmail.com>
2020-02-28 01:29:28 +0000

    Upload Edit Menu

---
+++ src/components/waiter/WaiterEdit.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const WaiterEditStyle = styled.div`
+  display: flex;
+`;
+
+export { WaiterEditStyle };
--- src/components/waiter/WaiterEdit.tsx
+++ src/components/waiter/WaiterEdit.tsx
@@ -1,7 +1,126 @@
 import React from "react";
+import {Button, Col, Container, Form, InputGroup, Navbar, Row} from "react-bootstrap";
+import {WaiterEditStyle} from "./WaiterEdit.styled";
 
 export default class WaiterEdit extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
-    return <h1>Edit Menu</h1>;
+    return (
+      <WaiterEditStyle>
+        <Container fluid className="mx-2">
+          <Navbar className="mb-5 mt-1" variant="dark" bg="dark">
+            <Navbar.Brand href="/#/">Oaxaca Waiter Edit</Navbar.Brand>
+          </Navbar>
+
+          <Row className="d-flex justify-content-center">
+            <Col xs="4">
+              <h2>Remove Menu Item</h2>
+              <Form onSubmit={() => console.log("MOCK")}>
+                <Form.Group controlId="itemID">
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        Menu Item ID:
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+                <Button variant="danger" type="submit">Remove</Button>
+              </Form>
+            </Col>
+            <Col xs="4">
+              <h2>Add Menu Item</h2>
+              <Form onSubmit={() => console.log("MOCK")}>
+                <Form.Group controlId="itemName">
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        Name
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="itemDescription">
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        Description
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="itemDescription">
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        Calories
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="itemDescription">
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        Allergens
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+
+                <Form.Group controlId="itemPrice">
+                  <InputGroup>
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        Price
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                    />
+                  </InputGroup>
+                </Form.Group>
+                <Button variant="success" type="submit">Add</Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </WaiterEditStyle>
+    )
   }
 }
