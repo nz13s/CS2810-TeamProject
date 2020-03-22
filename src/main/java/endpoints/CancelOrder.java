f9a86f2389e3f41876292fe1bc7a0a3315584d06
Bhavik Narang <bhavik.narang99@gmail.com>
2020-03-22 19:58:51 +0000

    Object mapper configured before being used to serialize the order.

--- src/main/java/endpoints/CancelOrder.java
+++ src/main/java/endpoints/CancelOrder.java
@@ -1,8 +1,34 @@
 package endpoints;
 
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import entities.Order;
+import entities.serialisers.OrderSerialiser;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-//TODO - Bhavik
+/*
+*
+* */
 public class CancelOrder extends HttpServlet {
+    private ObjectMapper om = new ObjectMapper();
+
+    /**
+     * Configures the {@link ObjectMapper} before being used to serialise the {@link Order} for the frontend, uses the {@link OrderSerialiser}
+     *
+     * @throws ServletException
+     */
+    @Override
+    public void init() throws ServletException {
+        om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        SimpleModule module =
+                new SimpleModule("OrderSerialiser", new Version(1, 0, 0, null, null, null));
+        module.addSerializer(Order.class, new OrderSerialiser());
+        om.registerModule(module);
+    }
+
 
 }
